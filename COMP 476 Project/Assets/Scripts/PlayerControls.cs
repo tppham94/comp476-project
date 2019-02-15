15227d1a004990116b8d0f7a2a330ac5421b0718
Markisino <tropiano.m@gmail.com>
2019-02-15 14:59:50 -0500

    Started Project
    - Added assets
    - Implemented Space Skybox
    - Implemented basic movement for player

---
+++ COMP 476 Project/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    Rigidbody _rb;
+    public float MovementSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("w"))
+            _rb.velocity += transform.forward * MovementSpeed;
+
+        if (Input.GetKey("s"))
+            _rb.velocity += transform.forward * -MovementSpeed;
+
+        if (Input.GetKey("a"))
+            transform.Rotate(Vector3.down);
+
+        if (Input.GetKey("d"))
+            transform.Rotate(Vector3.up);
+    }
+}
