d38ea5f20d5469c715722083ef8fe2e37e500ff6
n04x <ThomasBacks@gmail.com>
2019-04-04 19:21:13 -0400

    Added GameSetup and ShipAvatar
    Added GameSetup and ship avatar, will now branch it from here to play around Networking

---
+++ COMP 476 Project/Assets/Scripts/GameSetup.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+
+    #region Script Variables
+    private PhotonView PV;
+
+    public static GameSetup GS;
+    public Transform[] spawn_positions;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ COMP 476 Project/Assets/Scripts/Networking/ShipPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ShipPlayer : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject my_ship;
+    //List<int> spawn_indexes = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        int spawn_pos_pick = (PV.ViewID / 1000) - 1; // take the view id of the player and use it as spawn index position.
+        if(PV.IsMine)
+        {
+            my_ship = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "ShipAvatar"), GameSetup.GS.spawn_positions[spawn_pos_pick].position, GameSetup.GS.spawn_positions[spawn_pos_pick].rotation, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
