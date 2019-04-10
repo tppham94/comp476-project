983d27f335f7421ad0a7e79813c739168c07ebee
zigman32 <zigman32@gmail.com>
2019-04-10 15:02:53 -0400

    Added sounf effects to mother ship for taking damage + added a death animation

--- COMP 476 Project/Assets/Scripts/EnemyMothershipHealth.cs
+++ COMP 476 Project/Assets/Scripts/EnemyMothershipHealth.cs
@@ -8,6 +8,20 @@ public class EnemyMothershipHealth : MonoBehaviour
     public static EnemyMothershipHealth EMH;
     private PhotonView PV;
 
+    public AudioClip takedamage;
+    public AudioClip defeat;
+
+    public Vector3 deathfallposvector = new Vector3(1.0f, -2.0f, 1.0f);
+    public Vector3 deathfallanglevector = new Vector3(1.0f, 1.0f, 1.0f);
+
+    public float deathfallspeed = 50;
+    public float deathfallanglespeed = 100;
+
+    bool destroyed = false;
+    
+
+    AudioSource audioSource;
+
     public int health = 15;
 
     //private void OnTriggerEnter(Collider other)
@@ -32,16 +46,29 @@ public class EnemyMothershipHealth : MonoBehaviour
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         PV = GetComponent<PhotonView>();
     }
     void Update()
     {
-        if(PhotonNetwork.IsMasterClient)
-        {
-            if(health <= 0)
+        //if(PhotonNetwork.IsMasterClient)
+        //{
+            if(health <= 0 && !destroyed)
             {
-                PhotonNetwork.Destroy(this.gameObject);
+                audioSource.volume = 4.0f;
+                audioSource.pitch = 0.5f;
+                audioSource.PlayOneShot(defeat);
+                destroyed = true;
+                GetComponent<EnemyMothershipSpawn>().enabled = false;
+                
+                //PhotonNetwork.Destroy(this.gameObject);
             }
+        //}
+
+        if (destroyed)
+        {
+            transform.position += deathfallposvector * Time.deltaTime * deathfallspeed;
+            transform.Rotate(deathfallanglevector, deathfallanglespeed * Time.deltaTime);
         }
     }
 
@@ -49,9 +76,12 @@ public class EnemyMothershipHealth : MonoBehaviour
     {
         Debug.LogWarning(gameObject.name + " hp left: " + health);
         health--;
-      
+
+        audioSource.pitch = 3.5f;
+        audioSource.volume = 0.3f;
+        audioSource.PlayOneShot(takedamage);
         
-            PV.RPC("Health", RpcTarget.All, health);
+        PV.RPC("Health", RpcTarget.All, health);
        
     }
 
