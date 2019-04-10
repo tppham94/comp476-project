7f3be64fae505cd16c5a6470aa5f806c6f7ccc4b
tppham94 <43486951+tppham94@users.noreply.github.com>
2019-04-10 02:33:51 -0400

    PlayerSpeed display

---
+++ COMP 476 Project/Assets/Scripts/PlayerSpeedUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSpeedUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text speedUIText;
+    [SerializeField]
+    private float displaySpeed;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = displaySpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //displaySpeeds();
+        var playerControlsScript = GetComponent<PlayerControls>();
+        float mph = playerControlsScript._rb.velocity.magnitude * 2.237f;
+        speedUIText.text = Mathf.RoundToInt(mph).ToString() + " MPH";
+    }
+
+    void displaySpeeds()
+    {
+        
+    }
+}
